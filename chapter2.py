f6682d7b4f3044ae5ecc20db885e9f45274d035d
fath_ <142206210+faturalkhair@users.noreply.github.com>
2024-08-08 08:43:39 +0000

    chapter 2, bagian image

--- chapter2.py
+++ chapter2.py
@@ -1,7 +1,21 @@
 from PIL import Image
+from PIL import ImageFilter
 
 # Memuat gambar
-image = Image.open('owi.jpg')
-
+image = Image.open('gambar1.jpg')
 # Menyimpan gambar
-image.save('result.jpg')
+image.save('hasil_save.jpg')
+
+
+# Crop gambar
+cropped_image = image.crop((10, 10, 200, 200))
+cropped_image.save('hasil_crop.jpg')
+
+# Resize gambar
+resized_image = cropped_image.resize ((100,100))
+resized_image.save('hasil_resize.jpg')
+
+# Filter 
+filtered_image = resized_image.filter(ImageFilter.BLUR)
+filtered_image.save('hasil_filter.jpg')
+ 
